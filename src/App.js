bb18473d62d5fb7a460c8479c83eff1f91e8618f
RimanSIM <you@example.com>
2026-01-16 22:58:50 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to TradingFM Journal 🎉</h1>
+      <p>Your app is running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
